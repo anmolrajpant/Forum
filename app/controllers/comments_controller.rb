a42f8c0d52c61a40a7595d305918c0605e2459b3
Anmol <anmol.pant.c2@gmail.com>
2015-09-06 18:31:29 +0545

    comment added with controller and model

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,25 @@
+class CommentsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def delete
+  end
+
+  def destroy
+  end
+
+  def update
+  end
+
+  def edit
+  end
+end
